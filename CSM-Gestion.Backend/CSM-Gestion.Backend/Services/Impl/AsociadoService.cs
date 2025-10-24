2610702a25c7f69f3a7666da2538b8191b0bd054
blaz snk <blasasto0914@gmail.com>
2025-10-24 15:31:11 -0500

    agrege el patron unit of work

---
+++ CSM-Gestion.Backend/CSM-Gestion.Backend/Services/Impl/AsociadoService.cs
@@ -0,0 +1,15 @@
+﻿using CSM_Gestion.Backend.DTOs.Request;
+using CSM_Gestion.Backend.Service.Interface;
+using CSM_Gestion.Backend.Utils;
+
+namespace CSM_Gestion.Backend.Services.Impl
+{
+    public class AsociadoService : IAsociadoService
+    {
+
+        public Result<Guid> RegistrarFormulario(FormularioAsociadoRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
