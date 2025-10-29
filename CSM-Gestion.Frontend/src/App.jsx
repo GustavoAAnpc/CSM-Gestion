dc3872e0ff625a7634f34888eb243e886de70d2e
Gustavo Asto <gustavoblas2024@gmail.com>
2025-10-29 13:16:58 -0500

    agrege la variable de entorno

--- CSM-Gestion.Frontend/src/App.jsx
+++ CSM-Gestion.Frontend/src/App.jsx
@@ -1,18 +1,10 @@
-import { Routes, Route, NavLink } from "react-router-dom";
-import PerfilAsociado from "./pages/PerfilAsociado";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div className="p-6">
-      <nav className="mb-4">
-        <NavLink to="/" className="text-blue-600 font-bold">
-          Inicio
-        </NavLink>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<PerfilAsociado />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
--- CSM-Gestion.Frontend/src/api/asociadoService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5261/api/asociado"; // cambia puerto si es distinto
-
-export async function obtenerAsociado() {
-  const response = await axios.get(API_URL);
-  return response.data;
-}
