92298c32bf8b00066f1de0f26296f0313a9a4e32
blaz snk <blasasto0914@gmail.com>
2025-10-29 17:57:12 -0500

    realice la integracion del endpoint del login en el front

--- CSM-Gestion.Frontend/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-export default function HijosPage() {
-    return <>
-    
-    <h1>Hola Admin</h1>
-    
-    </>;
-}
---
+++ CSM-Gestion.Frontend/src/routes/protected.route.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { useNavigate, Outlet } from 'react-router-dom'
+import { BaseLayout } from '../shared/base.layout';
+
+export const ProtectedRoute = () => {
+    const navigate = useNavigate();
+    let isAuthenticated = false;
+
+    try {
+        const token = sessionStorage.getItem('admin');
+        if (token) {
+            const tokenParsed= JSON.parse(token);
+            isAuthenticated = !!tokenParsed;
+        }
+    }
+    catch (error) {
+        console.error("Error parsing user token from sessionStorage:", error);
+        isAuthenticate = false;
+    }
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/login', { replace: true });   
+        }
+
+    }, [isAuthenticated,navigate]);
+
+    if (!isAuthenticated) {
+        return null;
+    }
+
+    return (
+        <BaseLayout>
+            <Outlet />
+        </BaseLayout>
+    );
+}
