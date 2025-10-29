275b6f89ac0827d2c04cf019684d069a8005569e
blaz snk <blasasto0914@gmail.com>
2025-10-29 17:57:12 -0500

    realice la integracion del endpoint del login en el front

--- CSM-Gestion.Frontend/src/App.jsx
+++ CSM-Gestion.Frontend/src/App.jsx
@@ -1,23 +1,10 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
 
 function App() {
   const adminData = sessionStorage.getItem("admin");
   const isLoggedIn = adminData ? true : false;
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={isLoggedIn ? <Home /> : <Navigate to="/login" replace />}
-      />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/home"
-        element={isLoggedIn ? <Home /> : <Navigate to="/login" replace />}
-      />
-    </Routes>
+    <></>
   );
 }
 
--- CSM-Gestion.Frontend/src/pages/Home.jsx
+++ CSM-Gestion.Frontend/src/pages/Home.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-export default function Home() {
-  return (
-    <div className="p-8 text-center">
-      <h1 className="text-3xl font-bold">CSM Gestión</h1>
-      <p className="mt-4 text-lg">Bienvenido al frontend</p>
-    </div>
-  );
-=======
+
 export default function HijosPage() {
     return <>
     
     <h1>Hola Admin</h1>
     
     </>;
->>>>>>> c573350 (cambios en el frontend correccion que lo hice en el backend xd)
+
 }
