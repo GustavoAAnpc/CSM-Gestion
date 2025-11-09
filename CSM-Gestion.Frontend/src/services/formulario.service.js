3970001a93647a3a6a63bde2a0250365f7f99e84
blaz snk <blasasto0914@gmail.com>
2025-11-08 22:18:52 -0500

    ok por ahora va bien el registro

--- CSM-Gestion.Frontend/src/services/formulario.service.js
+++ CSM-Gestion.Frontend/src/services/formulario.service.js
@@ -1,6 +1,6 @@
     import axios from 'axios';
 
-    const API_BASE_URL = 'http://localhost:7156/api';
+    const API_BASE_URL = 'http://localhost:5261/api';
 
     const api = axios.create({
     baseURL: API_BASE_URL,
